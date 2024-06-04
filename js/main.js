110b16fd6ebdc1d8aa10305050dfd5cd463a16a8
KHD6 <168617446+KHD6@users.noreply.github.com>
2024-06-04 13:57:53 +0900

    0604_업데이트

--- js/main.js
+++ js/main.js
@@ -169,44 +169,59 @@ $(document).ready(function(){//시작
   }
 
   /*나타내기 시작*/
+  if($(window).width() < 1024){
+    $("#profile .right > ul > li").addClass("parallax__item__desc").addClass("reveal").addClass("reveal_LTR")
+  }else{
+    $("#profile .right > ul > li").addClass("parallax__item__desc").addClass("reveal").addClass("reveal_BTT")
+  }
+  $(window).resize(function(){
+    if($(window).width() < 1024){
+      $("#profile .right > ul > li").addClass("parallax__item__desc").addClass("reveal").addClass("reveal_LTR")
+    }else{
+      $("#profile .right > ul > li").addClass("parallax__item__desc").addClass("reveal").addClass("reveal_BTT")
+    }
+  });
+
+
   const hide = (item) => {
     gsap.set(item, {autoAlpha: 0});
-}
+  }
 
-const animate = (item) => {
-    let x = 0;
-    let y = 0;
-    let delay = item.dataset.delay;
+  const animate = (item) => {
+      let x = 0;
+      let y = 0;
+      let delay = item.dataset.delay;
 
-    if(item.classList.contains("reveal_LTR")){
-        x = -100,
-        y = 0
-    } else if(item.classList.contains("reveal_BTT")){
-        x = 0,
-        y = 100
-    } else if(item.classList.contains("reveal_TTB")){
-        x = 0,
-        y = -100
-    } else {
-        x = 100,
-        y = 0;
-    }
+      if(item.classList.contains("reveal_LTR")){
+          x = -100,
+          y = 0
+      } else if(item.classList.contains("reveal_BTT")){
+          x = 0,
+          y = 100
+      } else if(item.classList.contains("reveal_TTB")){
+          x = 0,
+          y = -100
+      } else {
+          x = 100,
+          y = 0;
+      }
 
-    gsap.fromTo(item, 
-        {autoAlpha: 0, x: x, y: y}, 
-        {autoAlpha: 1, x: 0, y: 0, delay: delay, duration: 1.25, overwrite: "auto", ease: "expo"}
-    );
-};
+      gsap.fromTo(item, 
+          {autoAlpha: 0, x: x, y: y}, 
+          {autoAlpha: 1, x: 0, y: 0, delay: delay, duration: 1.25, overwrite: "auto", ease: "expo"}
+      );
+  };
 
-gsap.utils.toArray(".reveal").forEach(item => {
-    hide(item);
+  gsap.utils.toArray(".reveal").forEach(item => {
+      hide(item);
 
-    ScrollTrigger.create({
-        trigger: item,
-        start: "top bottom",
-        end: "bottom top",
-        markers: false,
-        onEnter: () => {animate(item)}
-    });
-});
+      ScrollTrigger.create({
+          trigger: item,
+          start: "top bottom",
+          end: "bottom top",
+          markers: false,
+          onEnter: () => {animate(item)}
+      });
+  });  
+  /*나타내기 끝*/
 });//끝
