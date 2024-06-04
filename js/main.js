739135c553f8df2cbd29f4e170e2d3907243ea63
KHD6 <168617446+KHD6@users.noreply.github.com>
2024-06-04 11:26:03 +0900

    0604_업데이트

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ $(document).ready(function(){//시작
     title: "Photoshop",
     desc: "Illustrator"
   }, {
-    title: "Premiere",
+    title: "Premiere_Pro",
     desc: "After_Effects"
   }];
   var currentPage = 0;
